31599fb477d3a95b79b77883868b4d0140ef78e1
SenorPie <kaihanhakimi5@gmail.com>
2020-03-20 22:54:21 +0100

    Finished the index view, creating the task and listing the tasks on the index.html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -9,15 +9,32 @@ db = SQLAlchemy(app)
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    # Cant be left blank.
+
+    # Nullable means cant be left blank.
     content = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
+    def __repr__(self):
+        return f'<Task {self.id}>'
 
-@app.route('/')
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        # pass in the id of the input that you want to get the content of.
+        task_content = request.form['task_content']
+        new_task = Todo(content=task_content)
 
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+
+            return redirect('/')
+        except:
+            return 'An error occured, please retry.'
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
 
 if __name__ == '__main__':
     app.run(debug=True)
